b226fca85d98c29f4000a4df54eed5b064e7c1d5
sid00001 <siddharthchamoli61@gmail.com>
2022-02-25 01:14:30 +0530

    assinement-post-api

---
+++ src/routes/route.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+
+
+
+var play = [];
+var nameArray =[];
+
+router.post('/test-me', function (req, res) {
+    play.push(req.body)   // insert data in play array
+    res.send(play)   //send response to client side 
+    
+});
+router.post('/test-me2', function (req, res) {
+    if(nameArray.includes(req.body.name)){
+        return res.send('name Already exit')
+    }
+    
+    nameArray.push(req.body.name);
+
+    
+    play.push(req.body)   // insert data in play array
+
+    res.send(play)   //send response to client side 
+    
+});
+module.exports = router;
